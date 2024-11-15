cca3b5f38b7ac7cdefdbdde7fecb467201d6e13f
onsyi <150154418+onsyi@users.noreply.github.com>
2024-11-16 00:05:47 +0800

    Update anta.js

--- anta.js
+++ anta.js
@@ -1,37 +1,31 @@
-    // Fungsi Popup
-      function showPopup(popupId) {
-        const popup = document.getElementById(popupId);
-        if (popup) {
-          popup.style.display = "flex"; // Atau gunakan style yang sesuai
-        } else {
-          console.error(`Popup dengan ID ${popupId} tidak ditemukan.`);
-        }
-      }
-      
-      function hidePopup(popupId) {
-        const popup = document.getElementById(popupId);
-        if (popup) {
-          popup.style.display = "none";
-        } else {
-          console.error(`Popup dengan ID ${popupId} tidak ditemukan.`);
-        }
-      }
-      
-      // Contoh penggunaan popup
-      document.addEventListener("DOMContentLoaded", function () {
-        document.getElementById("showPopupButton").addEventListener("click", function () {
-          showPopup("examplePopup");
-        });
-      
-        document.getElementById("hidePopupButton").addEventListener("click", function () {
-          hidePopup("examplePopup");
-        });
-      });
-      
-     
-      // Produk Terlaris
+// Fungsi Popup
+function showPopup(popupId) {
+  const popup = document.getElementById(popupId);
+  if (popup) {
+    popup.style.display = "flex"; // Atau gunakan style yang sesuai
+  }
+}
+
+function hidePopup(popupId) {
+  const popup = document.getElementById(popupId);
+  if (popup) {
+    popup.style.display = "none";
+  }
+}
+
+// Contoh penggunaan popup
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("showPopupButton")?.addEventListener("click", function () {
+    showPopup("examplePopup");
+  });
+
+  document.getElementById("hidePopupButton")?.addEventListener("click", function () {
+    hidePopup("examplePopup");
+  });
+});
+
+// Produk Terlaris
 var PersenKu = 1100;
-var url_produk = "";
 var halaman = 1;
 var aktifasi = "terlaris"; // Tambahkan filter "terlaris"
 
@@ -44,33 +38,26 @@ $.ajax({
   },
   dataType: "JSON",
   success: function (data) {
-    try {
-      var xhtml = "";
-      data.forEach(function (element) {
-        var hargaNormal = element.Harga.substring(3).replace(/,/g, "");
-        var hargadiskon = Intl.NumberFormat().format(parseInt(hargaNormal * PersenKu + parseInt(hargaNormal)));
+    var xhtml = "";
+    data.forEach(function (element) {
+      var hargaNormal = element.Harga.substring(3).replace(/,/g, "");
+      var hargadiskon = Intl.NumberFormat().format(parseInt(hargaNormal * PersenKu + parseInt(hargaNormal)));
 
-        xhtml += '<a style="text-decoration:none" href="https://jgjk.mobi/p/' + element.Gambar.substr(-17, 13) + '">';
-        xhtml += '<div class="OnsyiCardo">';
-        xhtml += '<div class="OnsyiPromo1 terlaris-label"><i class="fa-solid fa-fire" style="font-size: 13px; color: orange;"></i> <span class="rating-number">Best Seller</span></div>';
-        xhtml += '<div class="thinn1">45% off</div>';
-        xhtml += '<img class="OnsyiImag3" src="' + element.Gambar + '"></div>';
-        xhtml += '<div class="product-details">';
-        xhtml += '<div class="OnsyiText-Judu">' + element.Nama + "</div>";
-        xhtml += '<div class="thinn">Terlaris - Harga terbaik</div>';
-        xhtml += '<div class="thinn2">Diantar secepatnya!</div>';
-        xhtml += '<div class="OnsyiText-Diskon"><del>Rp' + hargadiskon + "</del></div>";
-        xhtml += '<div class="OnsyiText-Harg">' + element.Harga + "</div>";
-        xhtml += '<txt class="cartu"><i class="fa-solid fa-bag-shopping" style="font-size: 17px;"></i></txt>';
-        xhtml += "</a></div>";
-      });
-      document.getElementById("prmo").innerHTML = xhtml;
-    } catch (err) {
-      alert("Terjadi Kesalahan. 01\n\n" + (err.message || "Error tidak diketahui."));
-    }
-  },
-  error: function (err) {
-    alert("Terjadi Kesalahan. 02\n\n" + (err.statusText || "Error tidak diketahui."));
+      xhtml += '<a style="text-decoration:none" href="https://jgjk.mobi/p/' + element.Gambar.substr(-17, 13) + '">';
+      xhtml += '<div class="OnsyiCardo">';
+      xhtml += '<div class="OnsyiPromo1 terlaris-label"><i class="fa-solid fa-fire" style="font-size: 13px; color: orange;"></i> <span class="rating-number">Best Seller</span></div>';
+      xhtml += '<div class="thinn1">45% off</div>';
+      xhtml += '<img class="OnsyiImag3" src="' + element.Gambar + '"></div>';
+      xhtml += '<div class="product-details">';
+      xhtml += '<div class="OnsyiText-Judu">' + element.Nama + "</div>";
+      xhtml += '<div class="thinn">Terlaris - Harga terbaik</div>';
+      xhtml += '<div class="thinn2">Diantar secepatnya!</div>';
+      xhtml += '<div class="OnsyiText-Diskon"><del>Rp' + hargadiskon + "</del></div>";
+      xhtml += '<div class="OnsyiText-Harg">' + element.Harga + "</div>";
+      xhtml += '<txt class="cartu"><i class="fa-solid fa-bag-shopping" style="font-size: 17px;"></i></txt>';
+      xhtml += "</a></div>";
+    });
+    document.getElementById("prmo").innerHTML = xhtml;
   },
 });
 
@@ -85,8 +72,8 @@ function gantiSaldo() {
 // Observer untuk mendeteksi perubahan pada elemen saldo
 var targetNode = document.getElementById("saldo");
 var config = { childList: true, subtree: true, characterData: true };
-var observer = new MutationObserver(function (mutationsList) {
-  mutationsList.forEach(gantiSaldo);
+var observer = new MutationObserver(function () {
+  gantiSaldo();
 });
 if (targetNode) observer.observe(targetNode, config);
 
@@ -161,36 +148,27 @@ function get_data() {
     method: "POST",
     dataType: "JSON",
     success: function (data) {
-      try {
-        let xhtml = "";
-        data.forEach(function (element) {
-          xhtml += '<div class="col-6 col-md-4 col-lg-3 d-flex justify-content-center">';
-          xhtml += '<a href="https://jgjk.mobi/p/' + element.Gambar.substr(-17, 13) + '">';
-          xhtml += '<div class="OnsyiCard3">';
-          xhtml += '<span class="OnsyiNew3">Best Seller</span>';
-          xhtml += '<img class="OnsyiImage3" src="' + element.Gambar + '">';
-          xhtml += '<div class="star3"><i class="fa-solid fa-star" style="color: #fff; font-size: 13px;"></i> 4.6 • 300+ rating</div>';
-          xhtml += '<h6 class="OnsyiText-Judul3">' + element.Nama + '</h6>';
-          xhtml += '<span class="thin3">Mitra Antarek</span>';
-          xhtml += '<div class="OnsyiText-Harga3">' + element.Harga + '</div>';
-          xhtml += '</div>';
-          xhtml += '</a></div>';
-        });
+      let xhtml = "";
+      data.forEach(function (element) {
+        xhtml += '<div class="col-6 col-md-4 col-lg-3 d-flex justify-content-center">';
+        xhtml += '<a href="https://jgjk.mobi/p/' + element.Gambar.substr(-17, 13) + '">';
+        xhtml += '<div class="OnsyiCard3">';
+        xhtml += '<span class="OnsyiNew3">Best Seller</span>';
+        xhtml += '<img class="OnsyiImage3" src="' + element.Gambar + '">';
+        xhtml += '<div class="star3"><i class="fa-solid fa-star" style="color: #fff; font-size: 13px;"></i> 4.6 • 300+ rating</div>';
+        xhtml += '<h6 class="OnsyiText-Judul3">' + element.Nama + '</h6>';
+        xhtml += '<span class="thin3">Mitra Antarek</span>';
+        xhtml += '<div class="OnsyiText-Harga3">' + element.Harga + '</div>';
+        xhtml += '</div>';
+        xhtml += '</a></div>';
+      });
 
-        document.getElementById("empat").innerHTML += xhtml;
+      document.getElementById("empat").innerHTML += xhtml;
 
-        if (data.length === 0) {
-          document.getElementById("selanjutnyaBtn").style.display = "none";
-          document.getElementById("lastPageMessage").style.display = "block";
-        }
-      } catch (err) {
-        console.error("Parsing Error:", err);
-        alert("Terjadi Kesalahan. 01\n\n" + err.message);
+      if (data.length === 0) {
+        document.getElementById("selanjutnyaBtn").style.display = "none";
+        document.getElementById("lastPageMessage").style.display = "block";
       }
-    },
-    error: function (err) {
-      console.error("Request Error:", err);
-      alert("Terjadi Kesalahan. 02\n\n" + err.responseText);
     },
   });
   halaman++;
